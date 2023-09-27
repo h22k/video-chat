69ab141697847ddb9acff8e208704b7b460bd526
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-09-27 06:13:11 +0300

    feat: created user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { UsersModule } from './users/users.module'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      driver: ApolloDriver,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
